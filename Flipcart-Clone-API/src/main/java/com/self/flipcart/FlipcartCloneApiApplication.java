bf1cff9ce424ff08f4278fa30dea9b80bb07435a
rajumb0232 <rajugowda0232@gmail.com>
2024-01-20 12:14:36 +0530

    project initiated

---
+++ Flipcart-Clone-API/src/main/java/com/self/flipcart/FlipcartCloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.self.flipcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipcartCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipcartCloneApiApplication.class, args);
+	}
+
+}
